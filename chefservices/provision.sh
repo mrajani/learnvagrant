e0e1e1aaa47497eeb2995a0077b20446f48e0d7c
mrajani <mahesh@ionosolutions.com>
2014-08-08 08:55:41 -0400

    added multi-vm vagfile

--- chefservices/provision.sh
+++ chefservices/provision.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo "Installating Apache2" > /tmp/out
-apt-get update >/dev/null 2>&1
-apt-get install -y apache2 >/dev/null 2>&1
+#apt-get update >/dev/null 2>&1
+#apt-get install -y apache2 >/dev/null 2>&1
