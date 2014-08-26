3b7b29ec583f91ecdb036b265ef4f2d80ba80b48
mrajani <mahesh@ionosolutions.com>
2014-08-26 13:54:32 -0700

    Puppet Configure

--- shared/puppetmaster.sh
+++ shared/puppetmaster.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+echo "$1 ${2}.iono.corp ${2} puppet" >> /etc/hosts
+
+sudo /bin/sed -i.old \
+  "s/localhost.localdomain/${2}.iono.corp/" /etc/sysconfig/network
+
+
+echo "Waiting for network service to restart"
+sudo service network restart
+sleep 15
+
 echo "Installing Puppet PuppetDB"
 # sudo rpm -i /shared/puppet-rpms/*.rpm
 sudo rpm -i http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
