7f8127fda0ca01e83d2301c060803dcec1cf5eba
mrajani <mahesh@ionosolutions.com>
2015-03-30 09:41:22 -0700

    ansisble, puppetmaster edits

--- shared/puppetmaster.sh
+++ shared/puppetmaster.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
-
-echo "$1 ${2}.${3} ${2} puppet" >> /etc/hosts
-
-sudo /bin/sed -i.old \
-  "s/localhost.localdomain/${2}.${3}/" /etc/sysconfig/network
-
-
-echo "Waiting for network service to restart"
-sudo service network restart
-sleep 15
-
 echo "Installing Puppet PuppetDB"
 # sudo rpm -i /shared/puppet-rpms/*.rpm
 sudo rpm -i http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
-sudo yum install -y puppet-server puppet
-   # puppetdb puppetdb-terminus
+
+sudo yum install -y puppet-server puppet puppetdb puppetdb-terminus
 
 echo "Installing MCollective"
 sudo yum install -y mcollective \
