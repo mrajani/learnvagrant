a93682d9463b3c6cd40e1387d9ba425f5409c262
mrajani <mahesh@ionosolutions.com>
2016-03-14 11:38:39 -0700

    fixing reporting tool scripts

--- shared/chefserver12.sh
+++ shared/chefserver12.sh
@@ -6,23 +6,22 @@ sudo yum update -y && sudo yum install ntp crontabs -y
 
 if [ -f $chefpkg ]; then
   sudo rpm -Uvh $chefpkg
-  # sudo rm $chefpkg
+  sudo rm $chefpkg
 fi
 
 sudo chef-server-ctl reconfigure
-sudo chef-server-ctl test
-sudo chef-server-ctl user-create vagrant vagrant vagrant 'vagrant@iono.corp' \
-  vagrant -f vagrant.pem
+# sudo chef-server-ctl test
 
-sudo chef-server-ctl install opscode-manage
+sudo chef-server-ctl install chef-manage
 sudo chef-server-ctl reconfigure
-sudo opscode-manage-ctl reconfigure
+sudo chef-manage-ctl reconfigure
 
 sudo chef-server-ctl install opscode-reporting
 sudo chef-server-ctl reconfigure
-sudo reporting-manage-ctl reconfigure
+sudo opscode-reporting-ctl reconfigure
 
-
+sudo chef-server-ctl user-create vagrant vagrant vagrant 'vagrant@iono.corp' \
+  vagrant -f vagrant.pem
 sudo chef-server-ctl org-create iono-org Iono_Org -a vagrant \
     -f iono-org-validator.pem
 
