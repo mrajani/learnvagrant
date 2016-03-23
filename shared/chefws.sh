0d9d8a8726230ad5e06f72aa27b3e927966035a5
mrajani <mahesh@ionosolutions.com>
2016-03-23 15:45:54 -0700

    adding berks, new commands to chef

--- shared/chefws.sh
+++ shared/chefws.sh
@@ -4,9 +4,17 @@
 #
 
 chefserver="chefserver12.iono.vm"
+chefdkpkg=/home/vagrant/${1}
+
+sudo yum check-update
+sudo yum update -y && sudo yum install ntp crontabs -y
 
 echo Installing Chef Client
-curl -L https://www.opscode.com/chef/install.sh | sudo bash
+if [ -f $chefdkpkg ]; then
+  sudo rpm -Uvh $chefdkpkg
+  sudo rm $chefdkpkg
+fi
+
 
 dotchefdir=/home/vagrant/.chef
 mkdir $dotchefdir; touch $dotchefdir/knife.rb
@@ -21,6 +29,21 @@ knife configure \
   --key /vagrant/vagrant.pem \
   --config .chef/knife.rb -y
 knife ssl fetch
+knife ssl check
 
 sudo knife client list
 sudo knife user list
+
+
+# debugging plural sight
+# chef verify
+# chef gem list chef-config
+# chef gem list chef-config -ra
+# knife cookbook create lab-linux
+# chef generate repo chef-repo
+# chef generate cookbook sample-cookbook
+# berks
+# berks install
+# berks upload -no-ssl-verify
+# knife node run_list add a49 'role[lab-linux]'
+# knife node run_list remove a49 'role[lab-linux]'
