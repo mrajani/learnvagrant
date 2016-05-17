8a855ab2fd7f9a0f80a751f261ca225c42c45561
Mahesh Rajani <mrajani@Maheshs-MacBook-Pro-2.local>
2016-05-17 10:52:12 -0700

    refactor ruby/yaml for vagrantfile

--- trusty/parseyaml.rb
+++ trusty/parseyaml.rb
@@ -4,12 +4,5 @@
 # read vm and chef configurations from JSON files
 require 'yaml'
 require 'pp'
-node = YAML.load_file(File.join(File.dirname(__FILE__), 'vagrant.yml'))[0]
-
-puts node['hostname']
-
-node['provisioners']['shell'] && node['provisioners']['shell'].each do |script|
-  puts "============="
-  puts script['path']
-  puts script['args'] if defined?(script['args'])
-end
+nodes = YAML.load_file(File.join(File.dirname(__FILE__), 'vagrant.yml'))['nodes']
+pp nodes
