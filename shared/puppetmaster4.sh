d1a53540c04bab6452380e7661a43b1e50411b27
mrajani <mahesh@ionosolutions.com>
2016-05-25 21:55:28 -0700

    yaml driven vagrantfile with vagrant.yml script to install puppet 4 puppet-agent pupperserver removed puppet 3

--- shared/puppetmaster4.sh
+++ shared/puppetmaster4.sh
@@ -3,38 +3,34 @@
 echo "Install Updates"
 # sudo yum update -y
 echo "Installing Puppet4 PuppetDB"
-sudo rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm
-sudo yum install -y puppet-agent puppetserver git ntp
+sudo yum install -y https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
+sudo yum repolist
+sudo yum install -y puppet-agent puppetserver ntp
 
-sudo chkconfig ntpd on
-sudo chkconfig --list ntpd
-sudo service ntpd start
+sudo systemctl enable ntpd
+sudo systemctl start ntpd
+sudo systemctl status ntpd
 
-## Adding new path to puppet in secure_path
+sudo firewall-cmd --list-all
+sudo firewall-cmd --add-port=8140/tcp --permanent --zone=public
+sudo firewall-cmd --add-service=ntp --permanent
+sudo firewall-cmd --reload
+sudo firewall-cmd --list-all
+
+
+## Adding  puppetlabs path to sudoers in secure_path when using sudo
 sudo sed -i "/^Defaults.*secure_path*/ s,$,:/opt/puppetlabs/bin," /etc/sudoers
 # From Learning Puppet 4
 #sed -i -e 's#\(secure_path = .*\)$#\1:/opt/puppetlabs/bin#' /etc/sudoers
 
-#sudo yum install puppetserver puppet puppetdb puppetdb-termini
-sudo puppet master --version
+
+# sudo puppet master --version
 sudo puppet resource package puppetdb ensure=latest
 sudo puppet resource service puppetdb ensure=running enable=true
 sudo puppet resource package puppetdb-termini ensure=latest
 sudo puppet config print confdir
 
-
-# echo "Installing MCollective"
-#sudo yum install -y mcollective \
-#  mcollective-client \
-#  java-1.6.0-openjdk \
-#  activemq \
-#  mcollective-puppet-agent \
-#  mcollective-puppet-client
-
-# For Production uses
-echo "Installing Apache and Phusion Passenger"
-sudo yum -y install httpd httpd-devel mod_ssl ruby-devel rubygems gcc \
-  gcc-c++ libcurl-devel openssl-devel
-
-## Needs centos-7 ruby 2.x or later is default passenger will not install
-sudo gem install rack passenger
+## Run puppet master for debugging
+## sudo /opt/puppetlabs/bin/puppet master --verbose --no-daemonize
+## wget http://apt.puppetlabs.com/puppetlabs-release-pc1-trusty.deb
+## puppet module install puppetlabs-mongodb --target-dir modules
