f88a81981f8cb0af5277f5f4152b6a5010bd45eb
mrajani <mahesh@ionosolutions.com>
2016-09-05 15:19:32 -0700

    modify puppetmaster4, add passenger script

--- shared/puppetmaster4.sh
+++ shared/puppetmaster4.sh
@@ -14,9 +14,10 @@ sudo systemctl status ntpd
 sudo firewall-cmd --list-all
 sudo firewall-cmd --add-port=8140/tcp --permanent --zone=public
 sudo firewall-cmd --add-service=ntp --permanent
+sudo firewall-cmd --add-service=http --permanent
+sudo firewall-cmd --add-service=https --permanent
 sudo firewall-cmd --reload
 sudo firewall-cmd --list-all
-
 
 ## Adding  puppetlabs path to sudoers in secure_path when using sudo
 sudo sed -i "/^Defaults.*secure_path*/ s,$,:/opt/puppetlabs/bin," /etc/sudoers
@@ -30,7 +31,39 @@ sudo puppet resource service puppetdb ensure=running enable=true
 sudo puppet resource package puppetdb-termini ensure=latest
 sudo puppet config print confdir
 
+## Add a Test environment
+## sudo mkdir -p /etc/puppetlabs/code/environments/test/{modules,hieradata,manifests}
+## sudo mkdir -p /etc/puppetlabs/code/hieradata
+
+## sudo puppet module install --environment test puppetlabs-ntp
+## puppet module list --environment test --tree
+
 ## Run puppet master for debugging
+## sudo groupadd puppet
+## sudo useradd -m -d /var/opt/puppetlabs -g puppet puppet
 ## sudo /opt/puppetlabs/bin/puppet master --verbose --no-daemonize
+
+## Install apache passenger
+## sudo yum install -y epel-release
+## sudo yum install -y httpd httpd-devel mod_ssl
+## curl -sSLo passenger.repo https://oss-binaries.phusionpassenger.com/yum/definitions/el-passenger.repo
+## mv passenger.repo /etc/yum.repos.d
+## sudo yum install -y passenger mod_passenger
+## sudo passenger-config validate-install --validate-apache2 --auto
+
+# sudo passenger-config validate-install --validate-apache2 --auto
+# cd /etc/httpd/conf.d/
+# for cfg in *.conf; do sudo mv $cfg ${cfg}.dis ; done
+# sudo cp /vagrant/etc-puppet/puppetmaster.conf /etc/httpd/conf.d
+# sudo mkdir -p /etc/puppetlabs/puppetmaster/public
+# sudo cp /vagrant/etc-puppet/config.ru /etc/puppetlabs/puppetmaster/
+# sudo chown puppet:puppet /etc/puppetlabs/puppetmaster/config.ru
+# sudo mkdir /var/run/puppetlabs/puppetmaster
+# sudo chown puppet:puppet /var/run/puppetlabs/puppetmaster
+# sudo mkdir /var/log/puppetlabs/puppetmaster
+# sudo chown puppet:puppet /var/log/puppetlabs/puppetmaster
+# sudo systemctl restart httpd
+# sudo systemctl status httpd
+
 ## wget http://apt.puppetlabs.com/puppetlabs-release-pc1-trusty.deb
 ## puppet module install puppetlabs-mongodb --target-dir modules
