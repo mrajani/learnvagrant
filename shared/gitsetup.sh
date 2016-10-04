384238d8207727974a560589d7521ec86e48be86
mrajani <mahesh@ionosolutions.com>
2016-10-04 16:13:16 -0700

    git setup for xenial copy git creds to /home/ubuntu, this is new for vagrant box ubuntu/xenial64 older versions and centos have user id vagrant and homedir /home/vagrant

--- shared/gitsetup.sh
+++ shared/gitsetup.sh
@@ -4,14 +4,21 @@
 # This file just installs github keys in known_hosts
 #
 
+user=vagrant
+os=${1}
+release=${2}
+if [[ ${release} == "xenial" ]]; then
+  user=ubuntu
+fi
+
 echo "Running ssh-keyscan"
 for host in 192.30.252.128 192.30.252.129 192.30.252.130 192.30.252.131 \
     github.com gitlab.com bitbucket.org
 do
-  ssh-keyscan ${host} >> /home/vagrant/.ssh/known_hosts
+  ssh-keyscan ${host} >> /home/${user}/.ssh/known_hosts
 done
 
-cp /shared/gitconfig /home/vagrant/.gitconfig
-cp /shared/ssh_id_rsa/* /home/vagrant/.ssh
-cp /shared/ssh-config /home/vagrant/.ssh/config
-sudo chown vagrant:vagrant /home/vagrant/.ssh/*
+cp /shared/gitconfig /home/${user}/.gitconfig
+cp /shared/ssh_id_rsa/* /home/${user}/.ssh
+cp /shared/ssh-config /home/${user}/.ssh/config
+sudo chown ${user}:${user} /home/${user}/.ssh/*
