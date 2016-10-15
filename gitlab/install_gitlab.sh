01c353ed45b65cc8dc04885e6d24051c84cb1de8
mrajani <mahesh@ionosolutions.com>
2016-10-14 20:24:24 -0700

    initial commit for gitlab

---
+++ gitlab/install_gitlab.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo ${1}
+echo ${2}
+
+if [[ ${1} =~ centos|redhat ]]; then
+  echo Found Centos/Redhat
+  sudo yum install -y -q curl policycoreutils openssh-server openssh-clients
+  sudo systemctl enable sshd
+  sudo systemctl start sshd
+  sudo yum install postfix
+  sudo systemctl enable postfix
+  sudo systemctl start postfix
+  sudo firewall-cmd --permanent --add-service=http
+  sudo systemctl reload firewalld
+else
+  echo Found Debian/ubuntu
+fi
