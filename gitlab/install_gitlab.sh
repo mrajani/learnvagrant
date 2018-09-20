bd7d30371cb2929e58a2545074b998c293d82c56
mrajani <mahesh@ionoinc.com>
2018-09-20 08:21:45 -0700

    modify gitlab config params, install script

--- gitlab/install_gitlab.sh
+++ gitlab/install_gitlab.sh
@@ -13,6 +13,9 @@ if [[ ${1} =~ centos|redhat ]]; then
   sudo systemctl start postfix
   sudo firewall-cmd --permanent --add-service=http
   sudo systemctl reload firewalld
+  curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh | sudo bash
+  sudo EXTERNAL_URL="http://192.168.128.58" yum install -y gitlab-ce
+
 else
   echo Found Debian/ubuntu
 fi
