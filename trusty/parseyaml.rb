a4d2471e08388e99b9426123ec3e43d9e6e9fa2e
mrajani <mahesh@ionoinc.com>
2018-09-30 14:51:35 -0700

    delete trusty folder

--- trusty/parseyaml.rb
+++ trusty/parseyaml.rb
@@ -5,4 +5,11 @@
 require 'yaml'
 require 'pp'
 nodes = YAML.load_file(File.join(File.dirname(__FILE__), 'vagrant.yml'))['nodes']
-pp nodes
+
+
+node = YAML.load_file(File.open("vagrant.yml"))['nodes']
+
+node['shared'].each do |sh|
+  puts sh
+  puts "/#{sh}"
+end
