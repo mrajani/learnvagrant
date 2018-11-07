37fb1c2c209a262d6f450ed8bf377b16b1c8de29
mrajani <mahesh@ionoinc.com>
2018-11-07 13:46:52 -0800

    modify gitinstall to identify os

--- shared/gitinstall.sh
+++ shared/gitinstall.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-os=${1}
-
+if [[ ( -z ${os} && -z ${dist} ) ]]; then
+  if [ -e /etc/os-release ]; then
+    . /etc/os-release
+    os=$( echo ${ID} | awk '{ print tolower($1) }')
+    dist=${VERSION_ID}
+  fi
+fi
+echo "Found ${os} "
 if [[ ${os} == "centos" ]]; then
+  sudo yum install -q -y epel-release.noarch
   sudo yum install -q -y git unzip
 elif [[ ${os} == "ubuntu" ]]; then
   sudo apt-get update && sudo apt-get install -y git
 else
-  exit
+  echo "Cannot identify the OS"
 fi
