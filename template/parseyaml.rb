5c161c56d8c2672a526b9c686175890a31b0a680
mrajani <mahesh@ionoinc.com>
2018-12-01 22:05:34 -0800

    update template dir

--- template/parseyaml.rb
+++ template/parseyaml.rb
@@ -4,12 +4,40 @@
 # read vm and chef configurations from JSON files
 require 'yaml'
 require 'pp'
-node = YAML.load_file(File.join(File.dirname(__FILE__), 'vagrant.yml'))[0]
+nodes = YAML.load_file(File.join(File.dirname(__FILE__), 'vagrant.yml'))['nodes']
 
-puts node['hostname']
+nodes.each do |node|
+  ports = node['provisioners']['ports'] # ports is an array
+  # pp ports
+  ports.each do |proto, value| # port is a hash
+      puts "#{node['hostname']} #{proto} - host #{value['host']} - guest #{value['guest']}"
+  end
 
-node['provisioners']['shell'] && node['provisioners']['shell'].each do |script|
-  puts "============="
-  puts script['path']
-  puts script['args'] if defined?(script['args'])
+  shells = node['provisioners']['shell'] # shells is an array
+  # pp shells
+  shells.each do |script, value| # shell is a hash
+    puts "#{script} #{value}"
+  end
+
+
+  dirs = node['provisioners']['mntdir'] # dirs is an array
+  # pp dirs
+  dirs.each do |mounted, value| # dir is a hash
+    puts "#{mounted} #{value}"
+  end
+
+  puts '================================'
+
+=begin
+  if node['provisioners']['mntdir']
+    mntdirs = node['provisioners']['mntdir']
+    mntdirs.each do |mntdir|
+      mntdir.each do |key, value|
+        puts "#{key} mounted on #{value}"
+      end
+      end
+    end=end
+    puts '================================'
+=end
 end
+
