6688e7910f2e1344d056adcd4cbacf9db63a78c4
mrajani <mahesh@ionoinc.com>
2019-05-19 10:11:33 -0700

    fix file copy in vm.provision enable linked clone in vagrantfile invoke powershell to install apps with chocolatey

--- windows/installChoco.ps1
+++ windows/installChoco.ps1
@@ -1,6 +1,6 @@
-#Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-#choco feature enable -n allowGlobalConfirmation
-#choco upgrade chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco feature enable -n allowGlobalConfirmation
+choco upgrade chocolatey
 cd "c:/tmp"
 echo $pwd
-choco install $pwd\apps.config
+powershell choco install $pwd\apps.config --no-progress
