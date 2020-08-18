aa4a7d78fbb5e66ec6cc33e2c382adcb136c4069
mrajani <mahesh@ionoinc.com>
2020-08-17 18:01:04 -0700

    delete parseyaml.rb

--- ubuntu/parseyaml.rb
+++
@@ -1,8 +0,0 @@
-# -*- mode: ruby -*-
-# vi: set ft=ruby :
-
-# read vm and chef configurations from JSON files
-require 'yaml'
-require 'pp'
-nodes = YAML.load_file(File.join(File.dirname(__FILE__), 'vagrant.yml'))['nodes']
-pp nodes
