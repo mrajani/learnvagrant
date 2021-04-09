520769013e61195beb026de93dd14c6478092cdd
mrajani <mahesh@ionoinc.com>
2021-04-08 22:08:09 -0700

    split playbook into smaller playbooks

--- focal/init.sh
+++ focal/init.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-sudo apt-get -y -q update
-sudo apt-get install -y python3-pip
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get -y -q update 2>&1 1>/dev/null
+echo Install python3-pip
+sudo apt-get install -y python3-pip 2>&1 1>/dev/null
+echo Install Ansible
 pip3 -qq install ansible pywinrm
