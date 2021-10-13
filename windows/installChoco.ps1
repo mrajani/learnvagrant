860c75e4e7e7c3fbb598ab113ad7703dfcf7170d
mrajani <laltopi@gmail.com>
2021-10-13 21:11:11 +0530

    setup region, homelocation, timezone to IN

--- windows/installChoco.ps1
+++ windows/installChoco.ps1
@@ -1,6 +1,10 @@
+Set-WinHomeLocation -GeoId 0x71
+Set-Culture en-IN
+Set-TimeZone -name "India Standard Time"
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco feature enable -n allowGlobalConfirmation
 choco upgrade chocolatey
 cd "c:/tmp"
 echo $pwd
 powershell choco install $pwd\apps.config --no-progress
+
